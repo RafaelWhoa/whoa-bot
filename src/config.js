f53fa11c920ef4104b8978c09dd8be0df4c3bbc4
Rafael Costa <44804@tjmt.jus.br>
2023-10-04 18:46:24 -0400

    feat: config slash commands

--- src/config.js
+++ src/config.js
@@ -1,21 +1,23 @@
-import "dotenv/config.js";
-import { REST, Routes} from "discord.js";
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
 
-const commands = [
-    {
-        name: 'ping',
-        description: 'Replies with Pong!',
+module.exports.initialConfig = async () =>{
+    const commands = [
+        {
+            name: 'ping',
+            description: 'Replies with Pong!',
+        }
+    ]
+
+    const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+    try {
+        console.log('Started refreshing application (/) commands.')
+
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
+
+        console.log('Successfully reloaded application (/) commands.')
+    }catch (e){
+        console.error(e)
     }
-]
-
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
-try {
-    console.log('Started refreshing application (/) commands.')
-
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
-
-    console.log('Successfully reloaded application (/) commands.')
-}catch (e){
-    console.error(e)
 }
