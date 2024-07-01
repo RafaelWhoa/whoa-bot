7fa0f27f502993417eda38b30f35c6ace5ddfe54
Rafael Costa <44804@tjmt.jus.br>
2024-06-30 21:23:29 -0400

    changed db connection data

--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    port: 5431,
+    host: 'db',
+    port: 5432,
     dialect: 'postgres',
     })
 
