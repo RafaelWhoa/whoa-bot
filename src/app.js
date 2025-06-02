78dcf9797b73532c097350df9664ca025877b45e
Rafael Costa <rafaelwhoa@gmail.com>
2025-06-01 21:16:57 -0400

    feat: Enviroment separation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 import {initialConfig} from './config.js';
 import logger from './logger.js';
+import path from "path";
 
-dotenv.config();
+const env = process.env.NODE_ENV || 'development';
+
+dotenv.config({path: path.resolve(process.cwd(), `.env.${env}`)});
 
 initialConfig().then(r => {logger.info('SlashCommands loaded')});
--- src/db/dbConfig.js
+++ src/db/dbConfig.js
@@ -1,11 +1,14 @@
 import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+const env = process.env.NODE_ENV || 'development';
+
+dotenv.config({path: path.resolve(process.cwd(), `.env.${env}`)});
 
 export const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'db',
-    port: 5432,
+    host: 'localhost',
+    port: 5433,
     dialect: 'postgres',
     })
 
